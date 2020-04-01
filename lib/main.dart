fc9e855fc60eed0cff54dc78f875d8d79d35bf60
Tanujdeep <yuvrajtanuj@rediffmail.com>
2020-04-01 18:51:17 +0530

    added a splash screen effect on initialising

--- lib/main.dart
+++ lib/main.dart
@@ -1,127 +1,152 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
 
 void main() {
-  runApp(MaterialApp(
-    home: FirstPage(),
+  runApp(
+    MaterialApp(
+      initialRoute: "splash",
+      routes: {
+        "splash": (context) => SplashScreen(),
+        "first": (context) => FirstPage(),
+        "second": (context) => SecondPage(),
+      },
     ),
   );
 }
 
-class FirstPage extends StatelessWidget{
+class SplashScreen extends StatefulWidget {
   @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(Duration(seconds: 5), () {
+      print("chal bey");
+      Navigator.pop(context);
+      Navigator.pushNamed(context, "first");
+    });
+  }
+
   Widget build(BuildContext context) {
     return Scaffold(
         backgroundColor: Colors.purple[300],
-        appBar: AppBar(
-          backgroundColor: Colors.deepPurple,
-          title: Text("Manual Mode"),
-          centerTitle: true,
+        body: Center(
+          child: CircleAvatar(
+            backgroundColor: Colors.pinkAccent,
+            radius: 50.0,
+            child: Icon(
+              Icons.directions_boat,
+              color: Colors.white,
+              size: 50.0,
+            ),
           ),
-        body: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Expanded(
-              child: Container(
-                color: Colors.pink[200],
-                child: Text(
-                  "Stream will run here",
-                  textAlign: TextAlign.center,
-                  ),
-              ),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: <Widget>[
-                
-                Container(
-                  
-                  child: IconButton(
-                    iconSize:20,
-                    onPressed: null,
-                    icon: Icon(Icons.arrow_drop_up) ),
-                ),
-                
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              crossAxisAlignment: CrossAxisAlignment.center ,
-              
-              children: <Widget>[
-                Container(
-                  
-                  child: IconButton(
-                    iconSize:20,
-                    onPressed: null,
-                    icon: Icon(Icons.arrow_left),
-                    ),
-                ),
-                Container(
-                  
-                  child: IconButton(
-                    iconSize:20,
-                    onPressed: null,
-                    icon: Icon(Icons.arrow_right),
-                    ),
-                ),
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center ,
-              
-              children: <Widget>[
-                Container(
-                  
-                  child: IconButton(
-                    iconSize:20,
-                    onPressed: null,
-                    icon: Icon(Icons.arrow_drop_down)),
-                ),
-              ],
-            ),
-          ],
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            Route routeToSecond= MaterialPageRoute(builder: (context)=>SecondPage());
-            Navigator.push(context,routeToSecond);
-            print("ekrjh");
-          },
-          child: Text("Mode"),
-          hoverColor: Colors.pink[800],
-          backgroundColor: Colors.purple[800],
-          ),
-      );
+        )
+        );
   }
 }
 
-class SecondPage extends FirstPage{
+class FirstPage extends StatelessWidget {
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Colors.purple[300],
-        appBar: AppBar(
-          backgroundColor: Colors.deepPurple,
-          title: Text("Automatic Mode"),
-          centerTitle: true,
+      backgroundColor: Colors.purple[300],
+      appBar: AppBar(
+        backgroundColor: Colors.deepPurple,
+        title: Text("Manual Mode"),
+        centerTitle: true,
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Expanded(
+            child: Container(
+              color: Colors.pink[200],
+              child: Text(
+                "Stream will run here",
+                textAlign: TextAlign.center,
+              ),
+            ),
           ),
-        body: Container(
-          child : Center(
-            child: Text("yahan stream hoyegi")
-          )
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            Route routeToFirst= MaterialPageRoute(builder: (context)=>FirstPage());
-            Navigator.pop(context,routeToFirst);
-          },
-          child: Text("Mode"),
-          hoverColor: Colors.pink[800],
-          backgroundColor: Colors.purple[800],
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Container(
+                child: IconButton(
+                    iconSize: 20,
+                    onPressed: null,
+                    icon: Icon(Icons.arrow_drop_up)),
+              ),
+            ],
           ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Container(
+                child: IconButton(
+                  iconSize: 20,
+                  onPressed: null,
+                  icon: Icon(Icons.arrow_left),
+                ),
+              ),
+              Container(
+                child: IconButton(
+                  iconSize: 20,
+                  onPressed: null,
+                  icon: Icon(Icons.arrow_right),
+                ),
+              ),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Container(
+                child: IconButton(
+                    iconSize: 20,
+                    onPressed: null,
+                    icon: Icon(Icons.arrow_drop_down)),
+              ),
+            ],
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.pushNamed(context, "second");
+        },
+        child: Text("Mode"),
+        hoverColor: Colors.pink[800],
+        backgroundColor: Colors.purple[800],
+      ),
     );
   }
-  
+}
+
+class SecondPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.purple[300],
+      appBar: AppBar(
+        backgroundColor: Colors.deepPurple,
+        title: Text("Automatic Mode"),
+        centerTitle: true,
+      ),
+      body: Container(child: Center(child: Text("yahan stream hoyegi"))),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.pop(context);
+        },
+        child: Text("Mode"),
+        hoverColor: Colors.pink[800],
+        backgroundColor: Colors.purple[800],
+      ),
+    );
+  }
 }
